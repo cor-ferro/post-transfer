c8f768051a3a7f75024dfd02f85823bc25ceb06e
thereissuchname <thereissuchname@yandex.ru>
2017-05-27 01:11:15 +0300

    build script

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,24 @@ const watch = require('gulp-watch');
 const plumber = require('gulp-plumber');
 const gutil = require('gulp-util');
 
+const paths = {
+	srcPattern: 'src/**/*.js',
+	dist: 'dist',
+};
+
 gulp.task('dev', () => {
-	gulp.src('src/**/*.js')
-		.pipe(watch('src/**/*.js'))
+	gulp.src(paths.srcPattern)
+		.pipe(watch(paths.srcPattern))
 		.pipe(plumber())
 		.pipe(babel())
-		.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest(paths.dist))
+		.on('error', gutil.log);
+});
+
+gulp.task('build', () => {
+	gulp.src(paths.srcPattern)
+		.pipe(plumber())
+		.pipe(babel())
+		.pipe(gulp.dest(paths.dist))
 		.on('error', gutil.log);
 });
