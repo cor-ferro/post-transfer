e609df26f07a9b4a6324b3060874572c6605ccc5
thereissuchname <thereissuchname@yandex.ru>
2017-06-01 22:01:53 +0300

    add log for promisesSome

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,17 @@
+import log from './logger';
+
 export function promisesSome(promises = []) {
 	return Promise
-		.all(promises.map(promise => promise.reflect()))
+		.all(promises.map((promise) => {
+			if (!promise.reflect) {
+				log.info('no promise.reflect');
+				log.info(promise);
+				console.log(promise);
+				return Promise.resolve(promise);
+			}
+
+			return promise.reflect();
+		}))
 		.filter(promise => promise.isFulfilled())
 		.map(promise => promise.value());
 }
